f773b6fdbde27f9dfb690ee7331198109771a779
zephyr <whatever@apoco.com>
2018-03-02 00:06:00 +0100

    initial commit

---
+++ ScryfallAPI/SimpleCommand.cs
@@ -0,0 +1,34 @@
+﻿namespace ScryfallAPI
+{
+    using System;
+    using System.Windows.Input;
+
+    public partial class MainWindow
+    {
+        public class SimpleCommand : ICommand
+        {
+            private Action _execute;
+            private Func<bool> _canExecute;
+            public SimpleCommand(Action execute, Func<bool> canExecute = null)
+            {
+                if (canExecute == null)
+                {
+                    canExecute = (() => true);
+                }
+                _execute = execute;
+                _canExecute = canExecute;
+            }
+            public event EventHandler CanExecuteChanged;
+
+            public bool CanExecute(object parameter)
+            {
+                return _canExecute();
+            }
+
+            public void Execute(object parameter)
+            {
+                _execute();
+            }
+        }
+    }
+}
